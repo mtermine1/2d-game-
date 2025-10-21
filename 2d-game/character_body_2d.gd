e6d005f3b75c52456c1a38157e174a8d332795c1
sage-autumn <srochetti@fordham.edu>
2025-10-21 14:06:20 -0400

    lots o things

--- 2d-game/character_body_2d.gd
+++ 2d-game/character_body_2d.gd
@@ -1,16 +1,21 @@
 extends CharacterBody2D
 
-
+const Bullet = preload("res://Bullet - Sage.tscn")
 const SPEED = 300.0
 const JUMP_VELOCITY = -800.0
 
 var camera_offset = 440
 
+@export var max_health = 50
+var health = max_health
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	if Input.is_action_just_pressed("attack"):
+		var newbullet = Bullet.instantiate()
+		add_child(newbullet)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -27,5 +32,13 @@ func _physics_process(delta: float) -> void:
 			camera_offset=-440
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+
+func damage(amount):
+	health -= amount
+	prints("health:", health)
+	if health <= 0:
+		health = 0
+	$AnimationPlayer.play("damage")
+	
+	
