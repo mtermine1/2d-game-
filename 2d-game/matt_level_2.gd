f45a61e3bc9313626afaf0ac2c7630a8fea9cfeb
mtermine1 <mtermine1@fordham.edu>
2025-10-23 12:41:44 -0400

    Matt

---
+++ 2d-game/matt_level_2.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var falling_obstacle_scene: PackedScene
+@export var spawn_interval := 2.0  # seconds between drops
+@export var spawn_x_range := Vector2(0, 2000)  # area across top of level
+
+var timer := 0.0
+
+func _process(delta):
+	timer += delta
+	if timer >= spawn_interval:
+		timer = 0
+		spawn_falling_obstacle()
+
+func spawn_falling_obstacle():
+	var obstacle = falling_obstacle_scene.instantiate()
+	add_child(obstacle)
+	obstacle.global_position = Vector2(
+		randf_range(spawn_x_range.x, spawn_x_range.y),
+		-100  # starts above the screen
+	)
