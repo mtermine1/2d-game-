6b96d01fef17f968f30e98f5c852156eff5d33b4
Grace <gjs1@fordham.edu>
2025-10-24 14:17:20 -0400

    2:17

--- 2d-game/grace's_scene.gd
+++ 2d-game/grace's_scene.gd
@@ -1,11 +1,24 @@
-extends Node2D
+extends CharacterBody2D
 
+@export var speed: float = 600.0
+@export var gravity_force: float = 600.0
+@export var throw_angle_degrees: float = -20.0  # negative = slightly upward
 
-# Called when the node enters the scene tree for the first time.
+var velocity: Vector2 = Vector2.ZERO
+
 func _ready() -> void:
-	pass # Replace with function body.
+	var angle = deg_to_rad(throw_angle_degrees)
+	velocity = Vector2(-speed * cos(angle), speed * sin(angle))
 
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity_force * delta
+	position += velocity * delta
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if position.x < -100 or position.x > 4000:
+		queue_free()
+
+# If you connect an Area2D's "area_entered" signal to this method:
+func _on_area_entered(area: Node) -> void:
+	if area is Node2D and area.is_in_group("player"):
+		area.call_deferred("damage", 1)  # call_deferred in case the player does something during the signal
+	queue_free()
