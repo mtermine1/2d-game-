105939bc30674a8f5fe83a51c589d35753f602b9
sage-autumn <srochetti@fordham.edu>
2025-10-29 11:34:59 -0400

    lots of scenes

---
+++ 2d-game/backhome.gd
@@ -0,0 +1,11 @@
+extends Button
+
+
+func _ready() -> void:
+	self.modulate.a = 0.0
+	# Connect the button's pressed signal to the function
+	self.pressed.connect(_on_pressed)
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://titlescene.tscn")
+	
